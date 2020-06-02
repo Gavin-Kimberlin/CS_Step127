393a8f8b0fc5072606c6ad468210c66cae31a80e
Gavin Kimberlin <guviner@gmail.com>
2020-06-01 18:24:41 -0700

    Create a program that concats, ToUppers, and StringBuilds.

--- CS_Step127/Program.cs
+++ CS_Step127/Program.cs
@@ -10,6 +10,26 @@ namespace CS_Step127
     {
         static void Main(string[] args)
         {
+            string s1 = "This ";
+            string s2 = "is a ";
+            string s3 = "string.\n";
+            Console.WriteLine(s1 + s2 + s3);
+
+
+            string lower = "uppercase\n";
+            Console.WriteLine(lower.ToUpper());
+
+            StringBuilder sb = new StringBuilder("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do ", 400);
+            sb.Append("eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut ");
+            sb.Append("enim ad minim veniam, quis nostrud exercitation ullamco laboris ");
+            sb.Append("nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in ");
+            sb.Append("reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla ");
+            sb.Append("pariatur. Excepteur sint occaecat cupidatat non proident, sunt in ");
+            sb.Append("culpa qui officia deserunt mollit anim id est laborum.");
+
+            Console.WriteLine(sb);
+
+            Console.ReadLine();
         }
     }
 }
